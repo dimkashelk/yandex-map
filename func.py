d708edde9990830ce4f2a734e155be4718386f77
shelk <shelk04@mail.ru>
2020-03-23 15:59:35 +0300

    Часть №2

--- func.py
+++ func.py
@@ -1,4 +1,21 @@
 import requests
+import sys
+
+
+def save_picture(addr, s):
+    map_request = f"http://static-maps.yandex.ru/1.x/?" \
+        f"ll={','.join(map(str, get_coords(addr)))}&" \
+        f"l=map&" \
+        f"z={s}"
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_f = f"{addr}.png"
+    with open(map_f, "wb") as file:
+        file.write(response.content)
 
 
 def get_coords(place):
--- main.py
+++ main.py
@@ -2,25 +2,33 @@ import sys
 import pygame
 from func import *
 
-response = None
+
 address = 'Тамбов ул. Мичуринская д. 112В'
-size = 0.001
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(map(str, get_coords(address)))}&spn={size},{size}&l=map"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-map_file = f"{address}.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+size = 10
+map_file = f'{address}.png'
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+save_picture(address, size)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEDOWN:
+                size -= 1
+                if size == -1:
+                    size = 0
+                else:
+                    print('Please wait...')
+                    save_picture(address, size)
+            elif event.key == pygame.K_PAGEUP:
+                size += 1
+                if size == 18:
+                    size = 17
+                else:
+                    print('Please wait...')
+                    save_picture(address, size)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
 pygame.quit()
